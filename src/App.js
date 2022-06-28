6a162c9b8867978ae5aaad8f01eec84aae6d0893
vakharwalad23 <105845275+vakharwalad23@users.noreply.github.com>
2022-06-29 03:12:38 +0530

    17

--- src/App.js
+++ src/App.js
@@ -8,13 +8,15 @@ import Navbar from './components/Navbar';
 import Home from './components/Home';
 import About from './components/About';
 import NoteState from './context/notes/NoteState';
+import Alert from './components/Alert';
 
 function App() {
   return (
     <>
       <NoteState>
         <Router>
-          <Navbar />
+          <Navbar/>
+          <Alert message="This is amazing"/>
           <div className='container'>
             <Routes>
               <Route exact path="/" element={<Home />}>
--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -4,7 +4,7 @@ import { useState } from "react";
 const NoteState = (props)=>{
    const notesInitial = [
     {
-      "_id": "62b8cd46b50de50360dac3f8",
+      "_id": "62b8cd46b50de50360dac3f81",
       "user": "62b74d2e3037ede74bedfc71",
       "title": "First Note",
       "description": "First Note in my api",
@@ -13,7 +13,7 @@ const NoteState = (props)=>{
       "__v": 0
     },
     {
-      "_id": "62b8cd46b50de50360dac3f8",
+      "_id": "62b8cd46b50de50360dac3f82",
       "user": "62b74d2e3037ede74bedfc71",
       "title": "Second Note",
       "description": "First Note in my api",
@@ -22,7 +22,7 @@ const NoteState = (props)=>{
       "__v": 0
     },
     {
-      "_id": "62b8cd46b50de50360dac3f8",
+      "_id": "62b8cd46b50de50360dac3f83",
       "user": "62b74d2e3037ede74bedfc71",
       "title": "Third Note",
       "description": "First Note in my api",
@@ -31,7 +31,7 @@ const NoteState = (props)=>{
       "__v": 0
     },
     {
-      "_id": "62b8cd46b50de50360dac3f8",
+      "_id": "62b8cd46b50de50360dac3f84",
       "user": "62b74d2e3037ede74bedfc71",
       "title": "Fourth Note",
       "description": "First Note in my api",
@@ -40,7 +40,7 @@ const NoteState = (props)=>{
       "__v": 0
     },
     {
-      "_id": "62b8cd46b50de50360dac3f8",
+      "_id": "62b8cd46b50de50360dac3f85",
       "user": "62b74d2e3037ede74bedfc71",
       "title": "Fifth Note",
       "description": "First Note in my api",
@@ -49,7 +49,7 @@ const NoteState = (props)=>{
       "__v": 0
     },
     {
-      "_id": "62b8cd46b50de50360dac3f8",
+      "_id": "62b8cd46b50de50360dac3f86",
       "user": "62b74d2e3037ede74bedfc71",
       "title": "Sixth Note",
       "description": "First Note in my api",
