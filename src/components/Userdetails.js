892dd6ab8ff20e38dae1a9557de81d2d8038f9cb
vakharwalad23 <105845275+vakharwalad23@users.noreply.github.com>
2022-07-06 02:12:24 +0530

    user name showing

--- src/components/Userdetails.js
+++ src/components/Userdetails.js
@@ -1,10 +1,18 @@
-import React from 'react'
+import React from 'react';
 
-const Userdetails = () => {
+const Userdetails = async () => {
+  const host = "http://localhost:5000";
+    // TODO: API Call
+    const response = await fetch(`${host}/api/auth/getuser`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'auth-token': localStorage.getItem('token')
+      }
+    });
+    const name = response.name;
   return (
-    <div>
-        
-    </div>
+    <div className='btn btn-default active'>{name}</div>
   )
 }
 
