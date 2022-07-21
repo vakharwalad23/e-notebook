7c6768c1cee2048a431b3a044ed7693ce93a456c
vakharwalad23 <105845275+vakharwalad23@users.noreply.github.com>
2022-07-22 02:48:25 +0530

    alert fix

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -10,7 +10,7 @@ const Alert = (props) => {
      }
    return (
      <div style={{height: '50px'}}>
-     {props.alert && <div className={`alert alert-${props.alert.type} alert-dismissible fade show`} role="alert">
+     {props.alert && <div className={`alert alert-${props.alert.type} alert-dismissible fade show`} role="alert" style={{width:"80%", margin:"auto"}}>
              <strong>{capitalize(props.alert.type)}</strong>: {props.alert.msg}
      </div>}
      </div>
